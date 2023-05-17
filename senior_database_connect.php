41871b55d3feb44fc70584cd2f7442d6327a7619
smuntazir <44447012+smuntazir@users.noreply.github.com>
2023-05-16 23:15:25 -0700

    senior_database_connect.php

--- senior_database_connect.php
+++ senior_database_connect.php
@@ -2,12 +2,12 @@
 
 
 // Create connection
-$db = mysqli_connect('localhost', 'root', 'root','project');
+$db = mysqli_connect('testdata1.cluster-crxvoye8t6bs.us-west-1.rds.amazonaws.com', 'admin', 'password','project');
 
 // Check connection
 if (!$db) {
   echo "Connection failed: " . mysqli_connect_error();
 } else {
-    echo "Connected successfully!";
+
 }
 ?>
