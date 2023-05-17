bb55e871666d221daf5d6c933c84c3897d3c18bb
Daniel Y Cheon <77998304+spantoport@users.noreply.github.com>
2023-05-17 02:07:46 -0700

    sign_up.php

--- sign_up.php
+++ sign_up.php
@@ -1,42 +1,4 @@
-<?php
 
-include("senior_database_connect.php"); 
-
-
-if (isset($_POST['sign_up'])) {
- 
-    if((isset($_POST['user_id'])) && (isset($_POST['user_pass']))) {
-    
-    $user_id = mysqli_real_escape_string($db,$_POST["user_id"]);
-    $user_pass= mysqli_real_escape_string($db, $_POST["user_pass"]);
-    
-    $pwd_error = "/^(?=.*[a-z])(?=.*\d)(?=.*[`~!@#$%^&*|\\\'\";:\.\,\/?=+\-_()<>{}])[a-z\d`~!@#$%^&*|\\\'\";:\.\,\/?=+\-_(){}<>]{8,}$/m";
-    $sql_injection = "/[`~!@#$%^&*|\\\'\";:\/?^=^+_()<>]/";
-        
-        if (empty($user_id)) {
-          
-            header("location: sign_up.php?error=you must put user name");
-        } 
-        else {
-          $user_pass = password_hash($user_pass, PASSWORD_DEFAULT);
-    
-    
-          $sql = "INSERT INTO `login_info`(`user_id`, `user_pass`) VALUES ('$user_id','$user_pass')";
-          $result = mysqli_query($db, $sql);
-          if ($result) {
-            echo "User information add in database";
-          } else {
-            echo "FAIL to add in database";
-    
-          }
-        }
-      }
-    }
-
-
-
-
-?>
 
 
 
